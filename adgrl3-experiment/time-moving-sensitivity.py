e3d73ddef59305326c73e9ab5ec7c5891c96595e
unnar19 <unnar19@ru.is>
2024-09-24 19:37:57 +0000

    split ligths on and off

--- adgrl3-experiment/time-moving-sensitivity.py
+++ adgrl3-experiment/time-moving-sensitivity.py
@@ -6,6 +6,7 @@ BASE_PATH = "adgrl3-clean-adjusted/"
 DATES = ["2024-04-21","2024-04-22"]
 EXPERIMENTS = [4,8]
 EXPERIMENTS = [[2,4],[2,4,6,8]]
+leg = [7,9,11,13,15,17]
 ALPH_MAP = ["A","B","C","D","E","F","G","H"]
 
 SPRINT_THRESHOLD_US = 15000
@@ -16,24 +17,28 @@ d = ","
 COMPARTMENTS = [f"{alph}{num}" for alph in ALPH_MAP for num in range(1,13)]
 NOISE = [f"{alph}{num}" for alph in ALPH_MAP for num in [0,13]]
 
-Time_moving_array = []
-Time_moving_array_on = []
-Time_moving_array_off = []
-Threshold_array = []
+plt.figure(figsize=(10, 8))
 
-SPRINT_THRESHOLD_US = 100
-max_threshold = 30001
-factor = 1.1
+legends = []
+line_count = 0
+for date_i, date in enumerate(DATES):
+    for exp_i in EXPERIMENTS[date_i]: 
 
-while SPRINT_THRESHOLD_US <= max_threshold:
+        Time_moving_array = []
+        Time_moving_array_on = []
+        Time_moving_array_off = []
+        Threshold_array = []
 
-    time_moving = 0
-    time_moving_on = 0
-    time_moving_off = 0
-    compartment_count = 0
+        SPRINT_THRESHOLD_US = 100
+        max_threshold = 30001
+        factor = 1.1
 
-    for date_i, date in enumerate(DATES):
-        for exp_i in EXPERIMENTS[date_i]: 
+        while SPRINT_THRESHOLD_US <= max_threshold:
+
+            time_moving = 0
+            time_moving_on = 0
+            time_moving_off = 0
+            compartment_count = 0
 
             for comp_id in COMPARTMENTS:
                 with open(f"{BASE_PATH}{date}/ex{exp_i}/compartments/{comp_id}.csv", mode='r') as file:
@@ -65,21 +70,35 @@ while SPRINT_THRESHOLD_US <= max_threshold:
 
                     compartment_count += 1
 
-    Time_moving_array.append(time_moving/(2*compartment_count))
-    Time_moving_array_on.append(time_moving_on/compartment_count)
-    Time_moving_array_off.append(time_moving_off/compartment_count)
-    Threshold_array.append(SPRINT_THRESHOLD_US)
 
-    SPRINT_THRESHOLD_US *= factor   
 
-plt.plot(Threshold_array, Time_moving_array)
-plt.plot(Threshold_array, Time_moving_array_on)
-plt.plot(Threshold_array, Time_moving_array_off)
-plt.xlabel("Threshold [µs]")
-plt.ylabel("Probability")
-plt.legend(["(Full recording)/2","Light on","Light off"])
-plt.title("ADGRL3.1 & WT - Sensitivity analysis of counting false positive sprints")
-plt.savefig("graphics\graphs\sensitivity\sensitivity.png")
+
+            # Time_moving_array.append(time_moving/(compartment_count))
+            Time_moving_array_on.append(time_moving_on/compartment_count)
+            Time_moving_array_off.append(time_moving_off/compartment_count)
+            Threshold_array.append(SPRINT_THRESHOLD_US)
+
+            SPRINT_THRESHOLD_US *= factor   
+
+
+        # plt.plot(Threshold_array, Time_moving_array)
+        plt.subplot(1,2,1)
+        plt.plot(Threshold_array, Time_moving_array_on)
+        plt.subplot(1,2,2)
+        plt.plot(Threshold_array, Time_moving_array_off)
+        legends.append(f"Recording {leg[line_count]}")
+        line_count += 1
+
+uh = ["on", "off"]
+for i in range(1,3):
+    plt.subplot(1,2,i)
+    plt.xlabel("Threshold [µs]")
+    plt.ylabel("Time moving [s]")
+    plt.grid()
+    plt.legend(legends)
+    plt.title(f"ADGRL3.1 & WT - Sensitivity analysis of time moving (Lights {uh[i-1]})")
+
+plt.savefig("graphics\graphs\sensitivity\sensitivity-tm.png")
 plt.show()
 
 
