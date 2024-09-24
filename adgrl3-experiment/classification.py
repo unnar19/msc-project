35aa6b740ec94be1d05bfb57ae5b449bcf74f67e
unnar19 <unnar19@ru.is>
2024-09-24 19:38:26 +0000

    add mean to histograms

--- adgrl3-experiment/classification.py
+++ adgrl3-experiment/classification.py
@@ -14,6 +14,8 @@ MUTANTS_ON_LEFT = [[1,0],[0,1,0,1]]
 WHOLE_TIME = 357
 SPIKE_TIME = 178
 
+TRAIN_RATIO = 0.85
+
 ec_mutant_on = []
 ec_mutant_off = []
 ec_wildtype_on = []
@@ -37,14 +39,15 @@ for alph in ALPH_MAP:
         comp_ids.append(f"{alph}{num}")
 
 def plot_histogram_bins(mutant_on, mutant_off, wildtype_on, wildtype_off,metric,ymax,bins,unit):
-    plt.figure(figsize=(10, 6))
+    fig = plt.figure(figsize=(10, 6))
     bins = np.histogram_bin_edges(mutant_on + mutant_off + wildtype_on + wildtype_off, bins=bins)
 
     plt.subplot(1, 2, 1)
     plt.hist(mutant_on, bins=bins, color="blue", alpha=0.5, rwidth=0.85, label="ADGRL3.1")
-    plt.axvline(x=np.mean(mutant_on), color="blue", label="ADGRL3.1 Mean")
+    # plt.axvline(x=np.mean(mutant_on), color="blue", label="ADGRL3.1 Mean")
     plt.hist(wildtype_on, bins=bins, color="red", alpha=0.5, rwidth=0.85, label="WT")
-    plt.axvline(x=np.mean(wildtype_on), color="red", label="WT Mean")
+    plt.axvline(x=np.mean(wildtype_on + mutant_on), color="k", label="Mean")
+    # plt.axvline(x=np.mean(wildtype_on), color="red", label="WT Mean")
     plt.ylim((0,ymax))
     plt.xlabel(f"{metric} bins [{unit}]")
     plt.ylabel("Count of larvae [#]")
@@ -53,9 +56,9 @@ def plot_histogram_bins(mutant_on, mutant_off, wildtype_on, wildtype_off,metric,
 
     plt.subplot(1, 2, 2)
     plt.hist(mutant_off, bins=bins, color="blue", alpha=0.5, rwidth=0.85, label="ADGRL3.1")
-    plt.axvline(x=np.mean(mutant_off), color="blue", label="ADGRL3.1 Mean")
+    # plt.axvline(x=np.mean(mutant_off), color="blue", label="ADGRL3.1 Mean")
     plt.hist(wildtype_off, bins=bins, color="red", alpha=0.5, rwidth=0.85, label="WT")
-    plt.axvline(x=np.mean(wildtype_off), color="red", label="WT Mean")
+    plt.axvline(x=np.mean(wildtype_off + mutant_off), color="k", label="Mean")
     plt.ylim((0,ymax))
     plt.xlabel(f"{metric} bins [{unit}]")
     plt.ylabel("Count of larvae [#]")
@@ -63,6 +66,7 @@ def plot_histogram_bins(mutant_on, mutant_off, wildtype_on, wildtype_off,metric,
     plt.legend()
 
     plt.tight_layout()
+    fig.savefig(f"graphics\graphs\histograms\histogram-{'-'.join(metric.lower().split())}.png")
     plt.show()
 
 for date_i, date in enumerate(DATES):
@@ -70,9 +74,15 @@ for date_i, date in enumerate(DATES):
 
         mutants_on_left = bool(MUTANTS_ON_LEFT[date_i][exp_i])
 
+        comp_count = 0
+
         for alph in ALPH_MAP:
             for num in range(1,13):
                 COMP_ID = f"{alph}{num}"
+
+                # detect if this compartment is train or test
+
+                comp_count += 1
                 if (mutants_on_left == True and num < 7) or (mutants_on_left == False and num >= 7):
                     is_mutant = True
                 else:
@@ -156,7 +166,6 @@ tm_threshold_on = np.mean([np.mean(tm_mutant_on), np.mean(tm_wildtype_on)])
 tm_threshold_off = np.mean([np.mean(tm_mutant_off), np.mean(tm_wildtype_off)])
 
 plot_histogram_bins(ec_mutant_on,ec_mutant_off,ec_wildtype_on,ec_wildtype_off,"Event count",90,50,"#")
-
 plot_histogram_bins(sc_mutant_on,sc_mutant_off,sc_wildtype_on,sc_wildtype_off,"Sprint count",160,50,"#")
 plot_histogram_bins(sd_mutant_on,sd_mutant_off,sd_wildtype_on,sd_wildtype_off,"Avg. sprint duration",45,50,"s")
 plot_histogram_bins(tm_mutant_on,tm_mutant_off,tm_wildtype_on,tm_wildtype_off,"Time moving",250,50,"s")
